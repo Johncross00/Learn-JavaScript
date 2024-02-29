1f5db9a1038b5de14d766aef4c9ceb45ed5d659b
johncross00 <johncross.ktm@gmail.com>
2024-02-29 08:07:20 +0000

    bi

--- test.js
+++ test.js
@@ -12,3 +12,12 @@ let place = 5
 place = place + 10
 console.log(place)
 
+let pers = monAge + monNom
+console.log(pers)
+
+let totalLivres = 500
+totalLivres += 50
+totalLivres -= 10
+totalLivres +=5
+let affichageTotalLivres = "Notre bibliothèque possède TOTAL livres" + totalLivres
+console.log(affichageTotalLivres)
